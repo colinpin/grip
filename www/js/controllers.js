e3e7803f52c2561a99018cdb49a2f1356ebfb7e2
Colin Pinegar <colinpin@gmail.com>
2016-03-14 21:02:59 -0600

    minor tweaks

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -77,16 +77,29 @@ angular.module('starter.controllers', [])
     })
 
     .controller('GripsCtrl', function($scope) {
-        $scope.grip = {
-            title: "MoMA Show",
-            locationCity: "New York City",
-            locationState: "NY",
-            locationCountry: "U.S.A.",
-            date: new Date,
-            image: "https://lh5.googleusercontent.com/-aiYNuzS3sQ0/AAAAAAAAAAI/AAAAAAAAAGw/U_s11kAXvXY/s0-c-k-no-ns/photo.jpg",
-            tags: ["Art", "Music", "Basquiat"],
-            gripper: [user],
-            viewers: []
-        }
+        $scope.grips = [
+            {
+                title: "MoMA Show",
+                locationCity: "New York City",
+                locationState: "NY",
+                locationCountry: "U.S.A.",
+                date: new Date(),
+                image: "https://lh5.googleusercontent.com/-aiYNuzS3sQ0/AAAAAAAAAAI/AAAAAAAAAGw/U_s11kAXvXY/s0-c-k-no-ns/photo.jpg",
+                tags: ["Art", "Music", "Basquiat"],
+                gripper: [user],
+                viewers: []
+            },
+            {
+                title: "MoMA Show",
+                locationCity: "New York City",
+                locationState: "NY",
+                locationCountry: "U.S.A.",
+                date: new Date(),
+                image: "https://lh5.googleusercontent.com/-aiYNuzS3sQ0/AAAAAAAAAAI/AAAAAAAAAGw/U_s11kAXvXY/s0-c-k-no-ns/photo.jpg",
+                tags: ["Art", "Music", "Basquiat"],
+                gripper: [user],
+                viewers: []
+            },
+        ]
     })
 
