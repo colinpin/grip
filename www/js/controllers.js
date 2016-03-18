c4524c71eda07dbfb6bd30a890dbe5bebad92fac
Colin Pinegar <colinpin@gmail.com>
2016-03-17 20:03:24 -0600

    hooking up firebase

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,28 +2,11 @@ angular.module('starter.controllers', [])
 
     .controller('DashCtrl', function($scope) { })
 
-    .controller('ChatsCtrl', function($scope, Chats) {
-        // With the new view caching in Ionic, Controllers are only called
-        // when they are recreated or on app start, instead of every page change.
-        // To listen for when this page is active (for example, to refresh data),
-        // listen for the $ionicView.enter event:
-        //
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
-
-        $scope.chats = Chats.all();
-        $scope.remove = function(chat) {
-            Chats.remove(chat);
-        };
-    })
-
-    .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-        $scope.chat = Chats.get($stateParams.chatId);
-    })
 
     .controller('AccountCtrl', function($scope) {
         //$scope.newUser = [];
         $scope.user = {
+            ID: "01234567890",
             firstName: "Chauncey",
             lastName: "Peppertooth",
             username: "see_pepper",
@@ -32,6 +15,8 @@ angular.module('starter.controllers', [])
             locationCity: "Boise",
             locationState: "Idaho",
             locationCountry: "U.S.A.",
+            email: "soandso@gmail.com",
+            password: "••••••••••",
             badges: [
                 {
                     name: "Pro",
@@ -44,6 +29,41 @@ angular.module('starter.controllers', [])
             ],
             rating: 3.5,
             grips: [
+                {
+                    title: "MoMA Show Opening",
+                    locationCity: "New York City",
+                    locationState: "NY",
+                    locationCountry: "U.S.A.",
+                    date: new Date(),
+                    image: "https://lh5.googleusercontent.com/-aiYNuzS3sQ0/AAAAAAAAAAI/AAAAAAAAAGw/U_s11kAXvXY/s0-c-k-no-ns/photo.jpg",
+                    tags: ["Art", "Music", "Basquiat"],
+                    gripper: "@username",
+                    viewers: "Archie"
+                },
+                {
+                    title: "People Arguing at Restaurant",
+                    locationCity: "New York City",
+                    locationState: "NY",
+                    locationCountry: "U.S.A.",
+                    date: new Date(),
+                    image: "http://images.teamsugar.com/files/upl0/0/3362/02_2008/AA032434.preview.jpg",
+                    tags: ["Art", "Music", "Basquiat"],
+                    gripper: "@username",
+                    viewers: "Archie"
+                },
+                {
+                    title: "Lionel Messi",
+                    locationCity: "Barcelona",
+                    locationState: "",
+                    locationCountry: "Spain",
+                    date: new Date(),
+                    image: "http://i.telegraph.co.uk/multimedia/archive/03054/messi1_3054545b.jpg",
+                    tags: ["Sports", "Soccer", "Messi"],
+                    gripper: "@username",
+                    viewers: "Archie"
+                },
+            ],
+            views: [
                 {
                     title: "MoMA Show",
                     locationCity: "New York City",
@@ -68,6 +88,34 @@ angular.module('starter.controllers', [])
                 },
             ],
             friends: [
+                {
+                    username: "boat_junk_boat",
+                    friendBadges: [
+                        {
+                            name: "Whatever",
+                            image: "http://www.bandanaworld.com/20108.JPG"
+                        },
+                        {
+                            name: "100Grips",
+                            image: "http://www.bandanaworld.com/20108.JPG"
+                        }],
+                    rating: 3.5,
+                    avatar: "https://lh5.googleusercontent.com/-aiYNuzS3sQ0/AAAAAAAAAAI/AAAAAAAAAGw/U_s11kAXvXY/s0-c-k-no-ns/photo.jpg"
+                },
+                {
+                    username: "charliez_angles",
+                    badges: [
+                        {
+                            name: "Doesn't Matter",
+                            image: "http://www.bandanaworld.com/20108.JPG"
+                        },
+                        {
+                            name: "Some other stuff",
+                            image: "http://www.bandanaworld.com/20108.JPG"
+                        }],
+                    rating: 3.5,
+                    avatar: "https://s-media-cache-ak0.pinimg.com/564x/8c/34/0f/8c340f41fc179452186fe68fea721849.jpg"
+                },
                 {
                     username: "boat_junk_boat",
                     friendBadges: [
@@ -125,5 +173,27 @@ angular.module('starter.controllers', [])
                 viewers: []
             },
         ]
+    })
+
+    .controller("signupCtrl", function($scope, $firebaseArray) {
+        var ref = new Firebase("https://grip.firebaseio.com/users");
+        $scope.users = $firebaseArray(ref);
+        // add new items to the array
+        // the user is automatically added to our Firebase database!
+        $scope.addUser = function() {
+           
+            if ($scope.newUserPassword === $scope.newUserPasswordConfirm) {
+                $scope.users.$add({
+                    firstName: $scope.newUserFirstName,
+                    lastName: $scope.newUserLastName,
+                    username: $scope.newUserUsername,
+                    avatar: "https://s-media-cache-ak0.pinimg.com/564x/a1/b6/38/a1b638245c1e90e9d578ff18fc19f03a.jpg",
+                    email: $scope.newUserEmail,
+                    password: $scope.newUserPassword
+                });
+            } else {
+                alert("Passwords do not match!")
+            }
+        };
     })
 
